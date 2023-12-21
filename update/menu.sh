3caea7b0e85146e7ac42b16cdf27e4d781ce36cc
muhamadferdiansyah <rapieryoru@gmail.com>
2023-12-21 08:53:46 +0700

    0

--- update/menu.sh
+++ update/menu.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 clear
 m="\033[0;1;36m"
-y="\033[0;1;37m"
-yy="\033[0;1;32m"
+y="\033[0;1;31m"
+yy="\033[0;1;35m"
 yl="\033[0;1;33m"
 wh="\033[0m"
 echo -e "$y-------------------------------------------------$wh"
-echo -e "$yy 1$y.  XRAY VLESS MENU$wh"
-#echo -e "$yy 2$y.  L2TP MENU$wh"
-#echo -e "$yy 3$y.  PPTP MENU$wh"
-#echo -e "$yy 4$y.  SSTP MENU$wh"
-#echo -e "$yy 5$y.  WIREGUARD MENU$wh"
-#echo -e "$yy 6$y.  SHADOWSOCKS MENU$wh"
-#echo -e "$yy 7$y.  SHADOWSOCKSR MENU$wh"
-#echo -e "$yy 8$y.  XRAY VMESS MENU$wh"
-echo -e "$yy 2$y.  SSH & OpenVPN MENU$wh"
-#echo -e "$yy 10$y. XRAY TROJAN MENU$wh"
-#echo -e "$yy 11$y. TROJAN GO MENU$wh"
-#echo -e "$yy 12$y. XRAY GRPC MENU$wh"
-#echo -e "$yy 13$y. SLOWDNS MENU (OFF)$wh"
-echo -e "$yy 3$y. CEK SEMUA IP PORT$wh"
-echo -e "$yy 4$y. CEK SEMUA SERVICE VPN$wh"
-echo -e "$yy 5$y. UPDATE MENU (Update)$wh"
-echo -e "$yy 6$y. sl-fix (Perbaiki Error SSLH+WS-TLS setelah reboot)$wh"
-echo -e "$yy 7$y. Settings (Pengaturan)$wh"
-echo -e "$yy 8$y. Exit (Keluar)$wh"
-echo -e "$yy 9$y. copyrepo (Salin Repo Script Mantap)$wh"
-echo -e "$yy 10$y. menuinfo (Untuk Mendapatkan Informasi)$wh"
-#echo -e "$yy 22$y. Shadowsocks Plugin (Buat Akun)$wh"
+echo -e "$yy 1.$y VLESS$wh"
+echo -e "$yy 2.$y SSH&OPENVPN$wh"
+echo -e "$yy 3.$y CEK SEMUA IP PORT$wh"
+echo -e "$yy 4.$y CEK SEMUA SERVICE VPN$wh"
+echo -e "$yy 5.$y UPDATE MENU (Update)$wh"
+echo -e "$yy 6.$y sl-fix (Perbaiki Error SSLH+WS-TLS setelah reboot)$wh"
+echo -e "$yy 7.$y Settings (Pengaturan)$wh"
+echo -e "$yy 8.$y Exit (Keluar)$wh"
+echo -e "$yy 9.$y copyrepo (Salin Repo Script Mantap)$wh"
+echo -e "$yy 10.$y menuinfo (Untuk Mendapatkan Informasi)$wh"
 echo -e "$y-------------------------------------------------$wh"
 read -p "Select From Options [ 1 - 10 ] : " menu
 case $menu in
@@ -35,54 +23,10 @@ case $menu in
 clear
 vlessmenu
 ;;
-#2)
-#clear
-#l2tpmenu
-#;;
-#3)
-#clear
-#pptpmenu
-#;;
-#4)
-#clear
-#sstpmenu
-#;;
-#5)
-#clear
-#wgmenu
-#;;
-#6)
-#clear
-#ssmenu
-#;;
-#7)
-#clear
-#ssrmenu
-#;;
-#8)
-#clear
-#vmessmenu
-#;;
 2)
 clear
 sshovpnmenu
 ;;
-#10)
-#clear
-#trmenu
-#;;
-#11)
-#clear
-#trgomenu
-#;;
-#12)
-#clear
-#grpcmenu
-#;;
-#13)
-#clear
-#slowdnsmenu
-#;;
 3)
 clear
 ipsaya
@@ -115,10 +59,6 @@ copyrepo
 clear
 menuinfo
 ;;
-#11)
-#clear
-#addss-p
-#;;
 *)
 clear
 menu
