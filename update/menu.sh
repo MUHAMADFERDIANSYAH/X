5e03242ed24f4b498040e6fd05b5ef6530779aba
muhamadferdiansyah <rapieryoru@gmail.com>
2023-12-21 09:21:43 +0700

    0

--- update/menu.sh
+++ update/menu.sh
@@ -5,7 +5,7 @@ y="\033[0;1;31m"
 yy="\033[0;1;35m"
 yl="\033[0;1;33m"
 wh="\033[0m"
-echo -e "$yl-------------------------------------------------$wh"
+#echo -e "$yl-------------------------------------------------$wh"
 echo -e "$yy 1.$y  VLESS$wh"
 echo -e "$yy 2.$y  SSH&OPENVPN$wh"
 echo -e "$yy 3.$y  CEK SEMUA IP PORT$wh"
@@ -15,8 +15,8 @@ echo -e "$yy 6.$y  SL-FIX (Perbaiki Error SSLH+WS-TLS setelah reboot)$wh"
 echo -e "$yy 7.$y  SETTINGS$wh"
 echo -e "$yy 8.$y  EXIT$wh"
 echo -e "$yy 9.$y  COPY REPO (Salin Repo Script Mantap)$wh"
-echo -e "$yy 10.$y MENU INFO (Untuk Mendapatkan Informasi)$wh"
-echo -e "$yl-------------------------------------------------$wh"
+#echo -e "$yy 10.$y MENU INFO (Untuk Mendapatkan Informasi)$wh"
+#echo -e "$yl-------------------------------------------------$wh"
 read -p "SELECT FROM OPTIONS [ 1 - 10 ] : " menu
 case $menu in
 1)
@@ -55,10 +55,10 @@ exit
 clear
 copyrepo
 ;;
-10)
-clear
-menuinfo
-;;
+#10)
+#clear
+#menuinfo
+#;;
 *)
 clear
 menu
