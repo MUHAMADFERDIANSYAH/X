7b7c18787834268c89253b012d851c8ecb3caa27
muhamadferdiansyah <rapieryoru@gmail.com>
2023-12-21 21:21:40 +0700

    0

--- xray/addvless.sh
+++ xray/addvless.sh
@@ -41,25 +41,14 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		fi
 	done
 uuid=$(cat /proc/sys/kernel/random/uuid)
-# Prompt user for maximum number of IP connections
 read -p "Max Ip login : " iplimit
-
-# Function to check and limit IP connections
 run_limit() {
-  # Clear vless log file
   echo -n > /var/log/vless/vless.log
-  
-  # Get list of users with IP limits from a file (assuming /etc/limit/vless/ip)
   data=( $(cat /etc/limit/vless/ip) )
-  
   for user in "${data[@]}"
   do
-    # Get current number of IP connections for the user from vless log file (assuming /var/log/xray/vless.log)
     ehh=$( grep "$user" /var/log/xray/vless.log | wc -l )
-    
-    # Check if the user has exceeded the maximum number of IP connections
     if [[ $(echo "$ehh" | wc -l) -gt $iplimit ]]; then
-      # Disable Trojan for the user and notify via Telegram (assuming nais is the Telegram bot username)
       disable-vless "$user" "$ehh" "$iplimit" "nais" "$(date "+%Y-%m-%d %H:%M:%S") $IPADDR $PROTOCOL $PORT $ALGORITHM $CIDR $UUID" >> /var/log/vless/vless.log
     else
       # Do nothing if the user has not exceeded the maximum number of IP connections
@@ -67,14 +56,6 @@ run_limit() {
     fi
     
     sleep 0.1 # Add a small delay to prevent resource exhaustion due to high concurrency of running this script multiple times simultaneously. Adjust as needed.
-  done
-  
-  # Check if any users have been disabled due to exceeding the maximum number of IP connections, and send a notification via Telegram (assuming nais is the Telegram bot username) if necessary (assuming log-vless is a log file containing vless-related events)
-  nais=3 # Set the number of times to retry sending the notification via Telegram due to possible network issues or bot downtime (adjust as needed)
-  while [[ $nais -gt 1 ]]; do
-    telegram-send --pre "$(cat log-vless)" > /dev/null || true # Try sending the notification via Telegram, and retry if it fails due to network issues or bot downtime (the "|| true" part prevents exiting the script due to an error message from telegram-send)
-    sleep 1 # Add a small delay between retries to prevent resource exhaustion due to high concurrency of running this script multiple times simultaneously. Adjust as needed.
-    nais=$((nais-1)) # Decrease the retry counter for each attempt. If it reaches zero, give up and exit the loop. (adjust as needed)
   done
 }
 read -p "Expired (Days) : " masaaktif
@@ -91,20 +72,20 @@ service cron restart
 clear
 echo -e ""
 echo -e "======-XRAYS/VLESS-======"
-echo -e "Remarks     : ${user}"
-echo -e "IP/Host     : ${MYIP}"
-echo -e "Address     : ${domain}"
-echo -e "Port TLS    : $tls"
-echo -e "Port No TLS : $nontls"
-echo -e "User ID     : ${uuid}"
-echo -e "Encryption  : none"
+echo -e "REMARKS    : ${user}"
+echo -e "IPHOST      : ${MYIP}"
+echo -e "ADDRESS    : ${domain}"
+echo -e "PORT TLS   : $tls"
+echo -e "PORT NTLS  : $nontls"
+echo -e "UUID        : ${uuid}"
+echo -e "Encryption   : none"
 echo -e "Network     : ws"
-echo -e "Path        : /vless/"
+echo -e "Path         : /vless/"
 echo -e "Created     : $hariini"
-echo -e "Expired     : $exp"
+echo -e "Expired      : $exp"
 echo -e "========================="
 echo -e "Link NTLS    : ${xrayvless1}"
 echo -e "========================="
 echo -e "Link TLS : ${xrayvless2}"
 echo -e "========================="
-echo -e "Script Mod By SL"
+echo -e "Script Mod By X"
