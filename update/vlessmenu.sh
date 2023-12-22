7c6f70841e88fc7a1534c8cdb60f2505fa16238f
muhamadferdiansyah <rapieryoru@gmail.com>
2023-12-22 09:48:10 +0700

    0

--- update/vlessmenu.sh
+++ update/vlessmenu.sh
@@ -5,7 +5,6 @@ y="\033[0;1;31m"
 yy="\033[0;1;35m"
 yl="\033[0;1;33m"
 wh="\033[0m"
-echo -e "$y                          VLESS $wh"
 echo -e "$yy 1.$y CREATE"
 echo -e "$yy 2.$y DELETE"
 echo -e "$yy 3.$y RENEW"
@@ -13,7 +12,6 @@ echo -e "$yy 4.$y USER LOGIN"
 echo -e "$yy 5.$y Menu"
 echo -e "$yy 6.$y EXIT"
 read -p "Select From Options [ 1 - 6 ] : " menu
-echo -e ""
 case $menu in
 1)
 addvless
