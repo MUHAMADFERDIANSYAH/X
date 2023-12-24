c553af454f2a6e73d572844073671a6397aeb6f7
muhamadferdiansyah <rapieryoru@gmail.com>
2023-12-24 15:20:52 +0700

    0

--- setup.sh
+++ setup.sh
@@ -18,28 +18,21 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
-# Link Hosting Kalian Untuk Ssh Vpn
-SSH="raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/ssh"
-# Link Hosting Kalian Untuk Xray
-XRAY="raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/xray"
-# Link Hosting Kalian Untuk Backup
-BACKUP="raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/backup"
-# Link Hosting Kalian Untuk Websocket
-WEBSOCKET="raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/websocket"
-# Link Hosting Kalian Untuk Ohp
-OHP="raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/ohp"
 
+rm setup.sh
 mkdir /var/lib/crot;
 echo "IP=" >> /var/lib/crot/ipvps.conf
-wget https://${SSH}/slhost.sh && chmod +x slhost.sh && ./slhost.sh
-#install xray
-wget https://${XRAY}/inxray.sh && chmod +x inxray.sh && screen -S xray ./inxray.sh
-#install ssh ovpn
-wget https://${SSH}/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-# Websocket
-wget https://${WEBSOCKET}/edu.sh && chmod +x edu.sh && ./edu.sh
-# Ohp Server
-wget https://${OHP}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+wget https://raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/ssh/slhost.sh && chmod +x slhost.sh && ./slhost.sh
+#XRAY
+wget https://raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/xray/inxray.sh && chmod +x inxray.sh && screen -S xray ./inxray.sh
+#SSH
+wget https://raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+#BR
+wget https://raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+#WS
+wget https://raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/websocket/edu.sh && chmod +x edu.sh && ./edu.sh
+#OHP
+wget https://raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/ohp/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -70,7 +63,7 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://${SSH}/set.sh"
+wget -O /etc/set.sh "https://raw.githubusercontent.com/MUHAMADFERDIANSYAH/X/main/ssh/set.sh"
 chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
